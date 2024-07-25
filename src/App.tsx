08d7f2b5a8a4dfa1eee6bce969073d90107762fb
gganj123 <48715999+gganj123@users.noreply.github.com>
2024-07-25 22:58:27 +0900

    type nuna

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Store from "./Store";
+import { Restaurant, Address } from "./model/resturant";
 
-function App() {
+let data: Restaurant = {
+  name: "누나네 식당",
+  category: "western",
+  address: {
+    city: "incheoi",
+    detail: "somewhere",
+    zipCode: 23425634,
+  },
+  menu: [
+    { name: "rose pasta", price: 2000, category: "PASTA" },
+    { name: "garlic steak", price: 3000, category: "STEAK" },
+  ],
+};
+
+const App: React.FC = () => {
+  const [myRestaurant, setMyRestaurant] = useState<Restaurant>(data);
+  const changeAddress = (address: Address) => {
+    setMyRestaurant({ ...myRestaurant, address: address });
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Store info={myRestaurant} changeAddress={changeAddress} />
     </div>
   );
-}
+};
 
 export default App;
