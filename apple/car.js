70e1ee6d3fcaca2ce0432e15fb569575eac2fff6
gganj123 <48715999+gganj123@users.noreply.github.com>
2024-08-29 22:29:20 +0900

    study : 문자,숫자별 타입 나눠서 각각 array에 넣기

--- apple/car.js
+++ apple/car.js
@@ -12,3 +12,27 @@ var car1 = new Car("쑤나타", 2000);
 var car2 = new Car("람보로", 20030);
 console.log(car2);
 console.log(car2.tax());
+var Word = /** @class */ (function () {
+    function Word() {
+        var params = [];
+        for (var _i = 0; _i < arguments.length; _i++) {
+            params[_i] = arguments[_i];
+        }
+        var numbers = [];
+        var strings = [];
+        params.forEach(function (i) {
+            if (typeof i === "string") {
+                strings.push(i);
+            }
+            else {
+                numbers.push(i);
+            }
+        });
+        this.num = numbers;
+        this.str = strings;
+    }
+    return Word;
+}());
+var obj = new Word("kim", 3, 5, "park");
+console.log(obj.num);
+console.log(obj.str);
--- apple/car.ts
+++ apple/car.ts
@@ -15,3 +15,27 @@ let car1 = new Car("쑤나타", 2000);
 let car2 = new Car("람보로", 20030);
 console.log(car2);
 console.log(car2.tax());
+
+class Word {
+  num: number[];
+  str: string[];
+  constructor(...params: (string | number)[]) {
+    let numbers: number[] = [];
+    let strings: string[] = [];
+
+    params.forEach((i) => {
+      if (typeof i === "string") {
+        strings.push(i);
+      } else {
+        numbers.push(i);
+      }
+    });
+    this.num = numbers;
+    this.str = strings;
+  }
+}
+
+let obj = new Word("kim", 3, 5, "park");
+
+console.log(obj.num);
+console.log(obj.str);
