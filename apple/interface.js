4a09688027a223e005b152168884750cc6e66b32
gganj123 <48715999+gganj123@users.noreply.github.com>
2024-08-30 17:34:30 +0900

    interface , type 차이점과 정리

---
+++ apple/interface.js
@@ -0,0 +1,9 @@
+//타입과 입터페이스의 차이점
+//interface는 중복선언 가능
+//type은 중복선언 불가능
+var Rec = {
+    color: "red",
+    width: 100,
+};
+var stu = { name: "kim", score: 40 };
+var tea = { name: "kim", age: 20, score: 40 };
---
+++ apple/interface.ts
@@ -0,0 +1,36 @@
+type Animal = { name: string };
+type Cat = { age: number } & Animal;
+//&은 extend랑 약간다름
+//두타입을 전부 만족하는 타입이라는 뜻!
+//& 쓸때 중복속성 발생하면?
+//Cat 에 age:string을 추가할때 처음에 에러는 안나지만,
+//생성할때 에러가 never로 뜬다. 미리주의!
+//결론은 ? interface가 좀더 안전하고 유연하다.
+interface Square {
+  color: string;
+  width: number;
+}
+//타입과 입터페이스의 차이점
+//interface는 중복선언 가능
+//type은 중복선언 불가능
+
+let Rec: Square = {
+  color: "red",
+  width: 100,
+};
+
+interface Student {
+  name: string;
+}
+//중복가능..
+interface Student {
+  score: number;
+}
+//이렇게 될 경우엔 두개의 데이터가 합쳐진다라고 보면됨 'extend효과!'
+//하지만 type은 엄격해서 안됨
+interface Teacher extends Student {
+  age: number;
+}
+
+let stu: Student = { name: "kim", score: 40 };
+let tea: Teacher = { name: "kim", age: 20, score: 40 };
