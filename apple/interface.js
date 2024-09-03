dc7e2e65b4634ffb049ec8e524f753a2d1e69299
gganj123 <zwol10@naver.com>
2024-09-03 11:59:01 +0900

    study : rest_param, destructuring, object

--- apple/interface.js
+++ apple/interface.js
@@ -7,3 +7,20 @@ var Rec = {
 };
 var stu = { name: "kim", score: 40 };
 var tea = { name: "kim", age: 20, score: 40 };
+var prod = {
+    brand: "Samsung",
+    serialNumber: 1360,
+    model: ["TV", "phone"],
+};
+var cart = [
+    { product: "청소기", price: 7000 },
+    { product: "삼다수", price: 800 },
+];
+var oper = {
+    plus: function (num1, num2) {
+        return num1 + num2;
+    },
+    minus: function (num1, num2) {
+        return num1 - num2;
+    },
+};
