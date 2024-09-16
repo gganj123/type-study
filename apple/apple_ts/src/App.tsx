5bdb27ff1c23760a5ca7f501c933f039ed7e367c
gganj123 <zwol10@naver.com>
2024-09-16 22:36:55 +0900

    study : react type 적용

--- apple/apple_ts/src/App.tsx
+++ apple/apple_ts/src/App.tsx
@@ -1,14 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
 let 박스 = <div></div>;
 function App() {
+  let [user, setUser] = useState<string | number>("kim");
+
   return (
     <div className="App">
       <h4>안녕하십니까</h4>
+      <Profile name="철수" age="20"></Profile>
     </div>
   );
+}
+
+function Profile(props: { name: string; age: string }): JSX.Element {
+  return <div>{props.name}입니다</div>;
 }
 
 export default App;
